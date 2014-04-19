275f1493de9c2cce0b886864f6dcd52ededc91c7
Satvik Andi <satvikandi@gmail.com>
2014-04-19 16:02:36 -0400

    Initiating Client and Server empty.

---
+++ src/Client.java
@@ -0,0 +1,14 @@
+
+public class Client {
+
+	public Client()
+	{
+	    System.out.println("THis is the sender in our Simple FTP implementation. \n");
+	}
+
+	public static void main(String args[])
+	{
+		Client cli = new Client();
+		//System.out.println("Hi");
+	}
+}
