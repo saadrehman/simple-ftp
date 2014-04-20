d955b0248fee38c70ca200c0e2708ffe28a14d3d
Saad Rehman Shah <this.saad@gmail.com>
2014-04-19 22:03:49 -0400

    Completes Client-end reading from the file and buffers the entire file.

--- src/Client.java
+++ src/Client.java
@@ -1,51 +1,118 @@
 import java.io.*;
+import java.util.LinkedList;
+//import java.util.ListIterator;
+import java.net.*;
 
-import com.sun.java_cup.internal.runtime.Scanner;
+
+
+
+
 
 // The client is the sender in our system
-
-
 public class Client {
 
 	int mss;
 	int window_size;
-	public Client()
-	{
-	    System.out.println("THis is the sender in our Simple FTP implementation. \n");
+	int window_start;
+	int window_end;
+	int nextPacket;
+	LinkedList<Packet> packet_buffer = new LinkedList<Packet>();
+	
+	private class Packet{
+		int sequence_num;
+		short checksum;
+		short type;
+		byte[] data = new byte[1000];//upper limit of data is MSS
 	}
+	
 
 	
-	// Main method gets user input.
+	
+	public Client(int window_size,int mss)	{
+		window_start = 0;
+		window_end = window_size - 1;
+		this.mss = mss;
+		nextPacket = 0;
+	}
+	
+	public void rdt_send(String filename, String server_host_name) throws Exception	{
+		// Reading data from the file
+		InputStream file_stream = new FileInputStream(new File(filename));
+		
+		//ListIterator<Packet> packet_buffer_iterator = packet_buffer.listIterator();
+		
+		byte[] chunk = new byte[mss]; //1 chunk of MSS bytes
+		int i = 0;
+		while(file_stream.read(chunk) != -1){
+			Packet p = new Packet();
+			p.sequence_num = i++;
+			p.type = 21845;		//Decimal equivalent of 0101010101010101
+			p.checksum = 21845;
+			p.data = chunk;
+			packet_buffer.add(p);
+			//TODO Put in the checksum functionality
+		}
+		file_stream.close();
+		
+		////////////////////////////////////////////////////////////////////////////s
+		//Reading the file into packet_buffer complete now. 
+		// Sending packet by packet not starting. 
+		
+		//int port;
+		//InetAddress address;
+		//DatagramSocket socket = null;
+		//DatagramPacket packet;
+		//byte[] sendBuf = new byte[256];
+		
+		Packet p = packet_buffer.pop();
+		
+	       // get a datagram socket
+        DatagramSocket socket = new DatagramSocket();
+
+            // send request
+        byte[] buf = new byte[mss + 64]; 
+        buf = p.data;
+        InetAddress address = InetAddress.getByName(server_host_name);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 7735);
+        socket.send(packet);
+    
+            // get response
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+
+	    // display response
+        String received = new String(packet.getData(), 0, packet.getLength());
+        System.out.println("Next Packet to Send: " + received);
+    
+        socket.close();
+
+		
+	}
+	
+
 	public static void main(String args[])
 	{
-		Client cli = new Client();
-		//System.out.println("Hi");
-		String server_host_name = "127.0.0.1";
-		int server_port_number = 7735;
-		String filename = ".//files//transfer_me.txt";
-		int window_size = 4;
-		int mss = 1000;
-		cli.window_size = window_size;
-		cli.mss = mss;
-		//System.out.println("Please enter the filename you want to transfer : \n");
+		if (args.length != 5) {
+		    System.out.println("Usage: java Client server-host-name server-port# file-name N MSS");
+		    return;
+		}
+		
+		String server_host_name = args[0];
+		int server_port = Integer.valueOf(args[1]);
+		String filename = ".//files//" + args[2];
+		int window_size = Integer.valueOf(args[3]);;
+		int mss = Integer.valueOf(args[4]); //bytes
+		
+		Client cli = new Client(window_size, mss);
+		System.out.println("Client is up and running!:\n");
+	
+
 		try {
-			rdt_send(filename,server_host_name);
+			cli.rdt_send(filename,server_host_name);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public static void rdt_send(String filename, String server_host_name) throws Exception
-	{
-		// Reading data from the file
-		// InputStream in = new FileInputStream(new File(filename));
-		BufferedReader buff = new BufferedReader(new FileReader(filename));
-	    String str;
-	    while ((str = buff.readLine()) != null)
-	        System.out.println(str);
-	   buff.close();
-	    //in.close
-		
-	}
+
 }
